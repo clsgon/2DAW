5a1b455664952773bf9753fd90fcd7db6c22eff6
clsgon <hercarlos@hotmail.es>
2023-10-13 09:28:14 +0200

    chat 2.0

--- servidor/php/ejercicios/chat/php/getMessage.php
+++ servidor/php/ejercicios/chat/php/getMessage.php
@@ -4,7 +4,11 @@ $message = $_POST["message"];
 $user = $_SESSION["user"];
 $path = "../csv/mensajes.csv";
 $file = fopen($path, "a");
-fwrite($file, "$user: $message\n");
+$hour = date('h:i:s');
+if(!strpos($message, "script") && !strpos($message, "alert"))
+    fwrite($file, "$user,$message,$hour\n");
+else
+    fwrite($file, "$user, el mensaje dio error, $hour\n");
 fclose($file);
 header("Location: ./chat.php");
 ?>
--- servidor/php/ejercicios/chat/php/signup.php
+++ servidor/php/ejercicios/chat/php/signup.php
@@ -7,22 +7,43 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" media="screen and (min-width: 1028px)" href="../style/signup.css">
+    <link rel="stylesheet" media="screen and (min-width: 480px) and (max-width: 1027px)" href="../style/signup.css">
+    <link rel="stylesheet" media="screen and (max-width: 480px)" href="../style/singupMobile.css">
     <title>Sign up</title>
 </head>
 <body>
     <form action="./check.php" method="post">
-        <label for="name">Nombre</label>
-        <input type="text" name="name" id="name">
-        <label for="surname">Apellidos</label>
-        <input type="text" name="surname" id="surname">
-        <label for="birthdate">Fecha de nacimiento</label>
-        <input type="date" name="birthdate" id="birthdate">
-        <label for="mail">Correo Electrónico</label>
-        <input type="email" name="mail" id="mail">
-        <label for="username">Nombre de Usuario</label>
-        <input type="text" name="username" id="username" required>
-        <label for="pass">Contraseña</label>
-        <input type="password" name="pass" id="pass" required>
+        <div class="group">
+            <div class="campo">
+                <label for="name">Nombre</label>
+                <input type="text" name="name" id="name" placeholder="Nombre">
+            </div>
+            <div class="campo">
+                <label for="surname">Apellidos</label>
+                <input type="text" name="surname" id="surname" placeholder="Apellidos">
+            </div>
+        </div>
+        <div class="group">
+            <div class="campo">
+                <label for="birthdate">Fecha de nacimiento</label>
+                <input type="date" name="birthdate" id="birthdate">
+            </div>
+            <div class="campo">
+                <label for="mail">Correo Electrónico</label>
+                <input type="email" name="mail" id="mail" placeholder="Correo">
+            </div>
+        </div>
+        <div class="group">
+            <div class="campo">
+                <label for="username">Nombre de Usuario</label>
+                <input type="text" name="username" id="username" required placeholder="Nombre de usuario">
+            </div>
+            <div class="campo">
+                <label for="pass">Contraseña</label>
+                <input type="password" name="pass" id="pass" required placeholder="Contraseña">
+            </div>
+        </div>
         <input type="submit" value="Registrarse">
     </form>
     <a href="./login.php">Ya tienes cuenta, inicia sesión!</a>
